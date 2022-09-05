886a847aca5b5447d7cc4de0d8e11d3737ac53e6
Svytoslav Soprun <svytsop@gmail.com>
2022-09-05 20:38:04 +0300

    modify App

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,20 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int Factorial(int a) {
+	int sum = 1;
+	for (size_t i = 1; i <= a; i++) {
+		sum *= i;
+	}
+	return sum;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	cout << "Enter: ";
+	int a;
+	cin >> a;
+	cout << Factorial(a);
+	system("pause");
+	return 0;
+}

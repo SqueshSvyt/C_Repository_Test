44898ba0db70b5c977cc249acda8d8379e18d2e9
Svytoslav Soprun <svytsop@gmail.com>
2022-09-05 23:02:52 +0300

    New_masfunction

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+const int Max_n = 100;
+
 int Factorial(int a) {
 	int sum = 1;
 	for (size_t i = 1; i <= a; i++) {
@@ -9,12 +11,27 @@ int Factorial(int a) {
 	}
 	return sum;
 }
+void  pullm(int mas[], int n) {
+	for (size_t i = 0; i < n; i++){
+		cout << "Enter numeber: ";
+		cin >> mas[i];
+		cout << endl;
+	}
+}
 
 int main() {
 	cout << "Enter a number for factorial: ";
 	int a;
 	cin >> a;
 	cout << Factorial(a) << endl;
+	cout << "Enter n of massive: " << endl;
+	int n = 0;
+	cin >> n;
+	int mas[Max_n];
+	pullm(mas, n);
+	for (size_t i = 0; i < n; i++) {
+		cout << "Your mass " << i << ": "<< mas[i] << endl;
+	}
 	system("pause");
 	return 0;
 }
